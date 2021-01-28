df35de6911ae644c3df96ff9310dbff0d8f71466
Jede Brahma <brahmajede@gmail.com>
2021-01-28 17:46:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,7 +35,7 @@ end
 def using_shift(array,string)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   im_so_over_this_city = my_favorite_cities.shift
-  end
+  
   p im_so_over_this_city
   
   p my_favorite_cities.size
