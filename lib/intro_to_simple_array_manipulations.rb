26b649dcaca9431f79679a6b7724baccafcfe319
Jede Brahma <brahmajede@gmail.com>
2021-01-28 16:42:45 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array, string)
+   array.push(string)
+ end 
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn","Queens","Manhattan","Bronx"]
+  updated_array = bouroughs_in_nyc.unshift("Staten Island")
+  new_neighborhood = updated_array[0]
+  p updated_array
+  p updated_array.size
+end
+  
+def 
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  deleted_string = continents.pop
+  p deleted_string
+  p continents.size
+end
+
+
+
+
