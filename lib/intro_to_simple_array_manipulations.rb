fb5982736d33072f4f17cf71da0ef9499c20f421
Jede Brahma <brahmajede@gmail.com>
2021-01-28 16:58:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,6 +19,27 @@ def using_pop(array, string)
   p deleted_string
   p deleted_string.size
 end
+def
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = dog_breeds.pop[2,3]
+  p small_dogs
+  p dog_breeds.length
+  
+end
+def
+   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+    im_so_over_this_city = my_favorite_cities.shift
+    p im_so_over_this_city
+    p my_favorite_cities.length
+ 
+end
+def 
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+    brands_removed = ice_cream_brands.shift[0,1]
+    p brands_removed
+    p brands_removed.length
+ 
+end
 
 
 
