220690b46a4fcb7b938b98093e7f8068d1f9e169
Jede Brahma <brahmajede@gmail.com>
2021-01-28 16:45:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,7 +5,7 @@ def using_push(array, string)
 def using_unshift(array, string)
   bouroughs_in_nyc = ["Brooklyn","Queens","Manhattan","Bronx"]
   bouroughs_in_nyc.unshift("Staten Island")
-  new_neighborhood = updated_array[0]
+  new_neighborhood = 
   p updated_array
   p updated_array.size
 end
